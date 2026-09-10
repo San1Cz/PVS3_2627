e78530c0439db880be5e9b6db96824e143c58f84
San1Cz <petr.matejka@infis.cz>
2026-09-10 12:38:33 +0200

    Delete src/Main.java

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-import fileworks.DataImport;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Nothing to see here.");
-        System.out.println("Peak hodina");
-    }
-}
